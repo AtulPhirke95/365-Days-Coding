8c9123aed7a983bdb4bf8ea340cb3feab5f6f335
AtulPhirke95 <40212505+AtulPhirke95@users.noreply.github.com>
2020-01-18 20:36:07 +0530

    Add files via upload

---
+++ January_2020/sum_of__two_numbers.py
@@ -0,0 +1,20 @@
+def return_me_the_indexes(n,arr):
+	"""
+		Input: n = 7
+			   arr = [2,3,7,4,12,35]
+		Output: (1, 3) -> 3 + 4 = 7 
+	"""
+	temp_lis = []
+	flag = False
+	for i in range(len(arr)):
+		if n - arr[i] in temp_lis:
+			flag = True
+			return arr.index(n - arr[i]), i
+		else:
+			temp_lis.append(arr[i])
+	if flag == False:
+		return "Not found"
+
+n = 7
+arr = [2,3,7,4,12,35]
+print(return_me_the_indexes(n,arr))
